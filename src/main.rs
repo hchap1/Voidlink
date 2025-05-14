3ac84dc74b4e87265c1424f381c26d4958fdcd4f
Harrison Chapman <hchap1@hotmail.com>
2025-05-14 13:15:31 +1000

    args test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    mode: String,
+
+    #[arg(short, long)]
+    ip: String,
+
+    #[arg(short, long, default_value_t = 7070)]
+    port: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("You said {} {} {}", args.mode, args.ip, args.port);
 }
